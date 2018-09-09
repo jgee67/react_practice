535802a5e3abcfb2ce4cb4d7f64d590bc195a9e0
Julian Gee <julian.gee@procore.com>
2018-09-09 15:33:10 -0700

    switch to webpacker

--- calendar/app/assets/javascripts/application.js
+++ calendar/app/assets/javascripts/application.js
@@ -12,10 +12,6 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require react
-//= require react_ujs
-//= require components
 //= require_tree .
 //= require jquery
 //= require jquery_ujs
---
+++ calendar/app/javascript/packs/appointments.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+export default class Appointments extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Calendar</h1>
+    );
+  }
+}
