fadac6461ce99ad6a00dd5f63addb525cfa01c9e
Julian Gee <julian.gee@procore.com>
2018-09-15 23:26:44 -0700

    import react datetime css

--- calendar/app/assets/javascripts/application.js
+++ calendar/app/assets/javascripts/application.js
@@ -10,8 +10,3 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require_tree .
-//= require jquery
-//= require jquery_ujs
